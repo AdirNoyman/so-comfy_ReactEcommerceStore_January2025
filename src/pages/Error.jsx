a2489e5a10c24973dc3a1dd092dea315e1d741da
Adir Noyman <ofahol2007@hotmail.com>
2025-01-06 21:21:02 +0200

    .

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,31 @@
+import { useRouteError, Link } from "react-router-dom"
+
 
 const Error = () => {
+
+  const error = useRouteError()
+  console.log(error)
+
+  if (error?.status === 404) {
+
+    return (
+      <main className="grid min-h-[100vh] place-items-center px-8">
+        <div className="text-center">
+          <p className="text-9xl font-semibold text-primary">404</p>
+          <h1 className="mt-4 tex-3xl font-bold tracking-tight sm:text-5xl">page not found</h1>
+          <p className="mt-6 text-lg leading-7">Sorry, we couldn&apos;t find the page you are looking for 🤷‍♂️ </p>
+          <div className="mt-10">
+          <Link to="/" className="btn btn-secondary">Back Home</Link>
+          </div>
+        </div>
+      </main>
+    )
+  }
+
   return (
-    <div>Error</div>
+    <main className="grid min-h-[100vh] place-items-center px-8">
+      <h4 className="text-center font-bold text-4xl">Sorry, there was an error 😩...</h4>
+    </main>
   )
 }
 
--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,7 +1,12 @@
+import { Outlet } from "react-router-dom"
 
 const HomeLayout = () => {
   return (
-    <div>HomeLayout</div>
+    <>
+    <nav className="text-4xl text-primary">SoComfy</nav>
+    {/* Outlet = the pages */}
+    <Outlet />
+    </>
   )
 }
 
